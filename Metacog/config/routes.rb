cc9b3236a74d4f07ddf3d400beb29efdce5a8d39
Michael Loubier <michael.a.loubier@gmail.com>
2016-11-16 16:17:51 -0800

    Added comments. Attributed to posts. Can delete comments. Edited formatting. Class 11-16.

--- Metacog/config/routes.rb
+++ Metacog/config/routes.rb
@@ -2,20 +2,36 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #Routes for posts
-  resources :posts, only: [:index, :show, :new, :create, :destroy, :edit]
-  # resources :posts
+  #Resources :posts
+  # resources :posts, only: [:index, :show, :new, :create, :destroy, :edit]
+  #Resources for :comments
+  # resources :comments, only: [:index, :new, :create, :destroy]
 
-  get '', to: "posts#index"
+  resources :posts, only: [:index, :show, :new, :create, :destroy, :edit] do
+      resources :comments, only: [:index, :new, :create, :destroy]
+  end
 
-  get "new", to: "posts#new"
+  root to: "posts#index"
 
-  post 'new', to: 'posts#create'
-
-  get 'posts/:id', to: 'posts#show'
-
-  get 'post/:id/edit', to: 'posts#edit'
-
-  patch 'posts/:id', to: 'posts#update'
+  # get '', to: "posts#index"
+  #
+  # get "new", to: "posts#new"
+  #
+  # post 'new', to: 'posts#create'
+  #
+  # get 'posts/:id', to: 'posts#show'
+  #
+  # get 'post/:id/edit', to: 'posts#edit'
+  #
+  # patch 'posts/:id', to: 'posts#update'
+  #
+  # get 'comments', to: 'comments#index'
+  #
+  # post 'comment', to: "comments#create"
+  #
+  # get 'posts/:id/newcomment', to: "comments#new"
+  #
+  # post 'posts/:id/newcomment', to: "comments#create", as: 'new_comment_path'
 
   #Routes for delete
 
