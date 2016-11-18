073daefffb5b60c4ddcaf1d50b57e5862a520643
Michael Loubier <michael.a.loubier@gmail.com>
2016-11-17 16:29:45 -0800

    Merging changes from Corey's fork.

--- Metacog/app/models/post.rb
+++ Metacog/app/models/post.rb
@@ -1,8 +1,5 @@
 class Post < ApplicationRecord
-<<<<<<< HEAD
-=======
-
->>>>>>> a9dc0c0fcc93cd1d1ac95629536b092354a9da72
+    
     has_many :comments, dependent: :destroy
     validates :title, :body, presence: true
 end
