27592340f2df82a5f732bc15aaf6347520a97483
coreyarch1234 <harrilal@mit.edu>
2016-11-17 16:35:10 -0800

    fixed merge conflicts

--- Metacog/app/controllers/comments_controller.rb
+++ Metacog/app/controllers/comments_controller.rb
@@ -22,14 +22,12 @@ class CommentsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:post_id])
-<<<<<<< HEAD
-    @comment =  @post.comments.find(params[:id])
-=======
+
     # @comment = @post.comments.new(comment_params)
     # @post.comments.find(params[:id]).destroy
     @comment = @post.comments.find(params[:id])
     @comment.destroy
->>>>>>> a9dc0c0fcc93cd1d1ac95629536b092354a9da72
+
     flash[:success] = "Comment deleted"
     redirect_to(@post)
   end
